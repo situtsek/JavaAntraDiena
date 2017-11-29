cc76780c8e6407617865d67ab6e68b7d5cd83160
situtsek <kestutis.milut@gmail.com>
2017-11-29 20:47:13 +0200

    Pirmos 9 uzduotys

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
         public static final int CHOISE_3 = 3;
         public static final int CHOISE_4 = 4;
         public static final int CHOISE_5 = 5;
+        public static final int CHOISE_6 = 6;
+        public static final int CHOISE_7 = 7;
+        public static final int CHOISE_8 = 8;
+        public static final int CHOISE_9 = 9;
+        public static final int CHOISE_10 = 10;
+
 
         public static void main(String[] args) {
             // parodyti vartotojui, kiek yra užduočių
@@ -29,7 +35,33 @@ import java.util.Scanner;
                     case CHOISE_1:
                         Uzduotis01 pirmoji = new Uzduotis01();
                         break;
-
+                    case CHOISE_2:
+                        Uzduotis02 antroji = new Uzduotis02();
+                        break;
+                    case CHOISE_3:
+                        Uzduotis03 trecioji = new Uzduotis03();
+                        break;
+                    case CHOISE_4:
+                        Uzduotis04 ketvirtoji = new Uzduotis04();
+                        break;
+                    case CHOISE_5:
+                        Uzduotis05 penktoji = new Uzduotis05();
+                        break;
+                    case CHOISE_6:
+                        Uzduotis06 sestoji = new Uzduotis06();
+                        break;
+                    case CHOISE_7:
+                        Uzduotis07 septintoji = new Uzduotis07();
+                        break;
+                    case CHOISE_8:
+                        Uzduotis08 astuntoji = new Uzduotis08();
+                        break;
+                    case CHOISE_9:
+                        Uzduotis09 devintoji = new Uzduotis09();
+                        break;
+                    case CHOISE_10:
+                        Uzduotis10 desimtoji = new Uzduotis10();
+                        break;
                 }
             }
         }
